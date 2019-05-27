0ffbb8e920287b5cd9527cbbbd5be0393fbf844c
DowForProxy <50099382+DowForProxy@users.noreply.github.com>
2019-05-27 12:15:31 +0200

    Add files via upload

--- Examples/projectmain.c
+++ Examples/projectmain.c
@@ -3,16 +3,30 @@
 
 int main()
 {
- int x,y;
- int *pointeurx,*pointeury;
- pointeurx=&x;
- pointeury=&y;
- printf("quelle est le nombre de salle maximal destine au TC?\n");
- scanf("%i",&x);
- printf("quelle est le nombre de matiere dispenser au TC?\n");
- scanf("%i",&y);
- int TELEVE[x];
- int TPROFESSEUR[y];
- int THEURE[y];
- conditioninitial(TELEVE,TPROFESSEUR,THEURE,pointeurx,pointeury);
+     int x,y;
+     int *pointeurx,*pointeury;
+     pointeurx=&x;
+     pointeury=&y;
+     FILE *fichier = NULL;
+     fichier = fopen("RecupCondIni.txt","r+");
+     if (fichier != NULL)
+     {
+         printf("quelle est le nombre de salle maximal destine au TC?\n");
+         scanf("%i",&x);
+         printf("quelle est le nombre de matiere dispenser au TC?\n");
+         scanf("%i",&y);
+         int TELEVE[x];
+         int TPROFESSEUR[y];
+         int THEURE[y];
+         conditioninitial(TELEVE,TPROFESSEUR,THEURE,pointeurx,pointeury);
+         fprintf(fichier,"le nombre de salle maximal est %i\n",x);
+         fprintf(fichier,"le nombre de matiere a dispenser en tc est de %i\n",y);
+         for(i=0)
+         fclose(fichier);
+     }
+     else
+     {
+        printf("Impossible d'ouvrir le fichier RecupCondIni");
+     }
+
 }
--- Examples/projeth.h
+++ Examples/projeth.h
@@ -1,5 +1,6 @@
 #ifndef PROJETH_H_INCLUDED
 #define PROJETH_H_INCLUDED
+
 void conditioninitial(int TELEVE[],int TPROFESSEUR[],int THEURE[],int *pointeurx,int *pointeury);
 
 #endif // PROJETH_H_INCLUDED
